b3be1e4a112a88c879fbadc9d0b123f7f2bc72e8
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-04-02 18:23:51 +0800

    Update formCard.js

--- components/formCard/formCard.js
+++ components/formCard/formCard.js
@@ -18,15 +18,27 @@ Component({
    * 组件的初始数据
    */
   data: {
-    maxRow: 0
+    maxRow: 0,
+    configLists: []
   },
-  observers: {
-    formConfigs(nval) {
-      this.setData({
-        maxRow: this._getMaxRow(nval) + 1
-      })
-    }
-  },
+  // observers: {
+  //   formConfigs(nval) {
+  //     const maxNum = this._getMaxRow(nval) + 1
+  //     console.log(maxNum)
+  //     let lists = []
+  //     for (let i = 0; i < maxNum; i++ ) {
+  //       lists = nval.filter(
+  //         setting => setting.layout.row === i && (!setting.showControlKey || this.data.formData[setting.showControlKey])
+  //       )
+  //       this.setData({
+  //         configLists: lists
+  //       })
+  //     }
+  //     this.setData({
+  //       maxRow: maxNum
+  //     })
+  //   }
+  // },
   /**
    * 组件的方法列表
    */
